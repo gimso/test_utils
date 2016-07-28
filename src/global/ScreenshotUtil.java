c77532adda135ef021833e9ad21fc9a91d30b3ea
yudag <Yehuda@simgo-mobile.com>
2016-07-28 11:44:51 +0300

    ScreenshutUtil: remove method takeScreenShot that receive TestUtil (deprecated) AUTO-146

--- src/global/ScreenshotUtil.java
+++ src/global/ScreenshotUtil.java
@@ -12,15 +12,14 @@ import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
-import testrail.api.TestRailAPI;
-
 public class ScreenshotUtil {
+	
 	/**
-	 * Get the test name from the class instance, </br> If not exist create a
-	 * Screenshot folder,</br> Create an inner file with the test name and
-	 * date+time .png,</br> Create a screenshot of the current screen using
-	 * method: </br> &nbsp &nbsp &nbsp
-	 * org.openqa.selenium.TakesScreenshot.driver
+	 * Get the test name from the class instance, </br>
+	 * If not exist create a Screenshot folder,</br>
+	 * Create an inner file with the test name and date+time .png,</br>
+	 * Create a screenshot of the current screen using method: </br>
+	 * &nbsp &nbsp &nbsp org.openqa.selenium.TakesScreenshot.driver
 	 * .getScreenshotAs(OutputType.FILE)</br>
 	 * 
 	 * @param String
@@ -28,20 +27,16 @@ public class ScreenshotUtil {
 	 */
 	public static void takeScreenShot(String testName) {
 		File userDir = new File(System.getProperty("user.dir") + "\\Screenshots");
-		
+
 		if (!userDir.exists())
 			userDir.mkdir();
 
-		File srcFile = ((TakesScreenshot) PersistUtil.getInstance().getDriver())
-				.getScreenshotAs(OutputType.FILE);
-		String date = new SimpleDateFormat("ddMMyy-HH_mm_ss_SSS")
-				.format(new Date());
+		File srcFile = ((TakesScreenshot) PersistUtil.getInstance().getDriver()).getScreenshotAs(OutputType.FILE);
+		String date = new SimpleDateFormat("ddMMyy-HH_mm_ss_SSS").format(new Date());
 
 		try {
-			FileUtils.copyFile(srcFile, new File(userDir, testName + "_" + date
-					+ ".png"));
-			System.err.println("Test failed, screenshot saved in " + userDir
-					+ "\\" + testName + "_" + date + ".png");
+			FileUtils.copyFile(srcFile, new File(userDir, testName + "_" + date + ".png"));
+			System.err.println("Test failed, screenshot saved in " + userDir + "\\" + testName + "_" + date + ".png");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -50,37 +45,17 @@ public class ScreenshotUtil {
 	/**
 	 * Save the screenshot file in the local output file and print the path to
 	 * the console.</br>
-	 * 
-	 * @param ITestResult
-	 */
-	public static void takeScreenShot(ITestResult result, String testNameField) {
-		String testName = "cannot get the test name";
-		Object instance = result.getInstance();
-		try {
-			testName = (String) instance.getClass()
-					.getDeclaredField(testNameField).get(instance);
-		} catch (IllegalArgumentException | IllegalAccessException
-				| NoSuchFieldException | SecurityException e) {
-			try {
-				testName = TestRailAPI.getInstance().getMethodName();
-			} catch (Exception e1) {
-			}
-		}
-		
-		ScreenshotUtil.takeScreenShot(testName);
-	}
-
-	/**
-	 * Save the screenshot file in the local output file and print the path to
-	 * the console.</br> the name file will come from method name
+	 * the name file will come from method name
 	 * 
 	 * @param ITestResult
 	 */
 	public static void takeScreenShot() {
 		ScreenshotUtil.takeScreenShot(getMethodName());
 	}
+
 	/**
 	 * get from testng method the method name
+	 * 
 	 * @return
 	 */
 	private static String getMethodName() {
