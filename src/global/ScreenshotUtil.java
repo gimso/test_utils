29a0e76e4a17a877c6b55dfef90ba8c659a1c404
orshachar <orshachar23@gmail.com>
2016-08-21 16:32:06 +0300

    Fix a bug in ScreenshotUtil

--- src/global/ScreenshotUtil.java
+++ src/global/ScreenshotUtil.java
@@ -12,6 +12,8 @@ import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
+import testrail.api.TestRailAPI;
+
 public class ScreenshotUtil {
 	
 	/**
@@ -40,7 +42,31 @@ public class ScreenshotUtil {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
+	
+	/**
+  	 * Save the screenshot file in the local output file and print the path to
+  	 * the console.</br>
+ 	 * 
+ 	 * @param ITestResult
+ 	 */
+	public static void takeScreenShot(ITestResult result, String testNameField) {		
+   	String testName = "cannot get the test name";		
+ 		Object instance = result.getInstance();		
+ 		try {		
+ 			testName = (String) instance.getClass()		
+ 					.getDeclaredField(testNameField).get(instance);		
+ 		} catch (IllegalArgumentException | IllegalAccessException		
+ 				| NoSuchFieldException | SecurityException e) {		
+ 			try {		
+ 				testName = TestRailAPI.getInstance().getMethodName();		
+ 			} catch (Exception e1) {		
+ 			}		
+ 		}		
+ 				
+ 		ScreenshotUtil.takeScreenShot(testName);		
+ 	}
 
 	/**
 	 * Save the screenshot file in the local output file and print the path to
